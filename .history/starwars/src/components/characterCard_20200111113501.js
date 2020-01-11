fd184034d754f7f9259de795c6ed64658876e5a6
Gerardo Cazares <grcazares@gmail.com>
2020-01-11 11:54:10 -0800

    Final
    Installed testing libray, did not get to use it due to being unfamiliar with documentation. Converted kilograms from API into pounds and somehwhat converted meters to lbs, will  figure out proper way to display inches after submitting Sprint Challenge.

--- .history/starwars/src/components/characterCard_20200111113501.js
+++ .history/starwars/src/components/characterCard_20200111113501.js
@@ -18,8 +18,8 @@ const CharCard = props => {
         <Card>
             <p><strong>Name:</strong> {props.name}</p>
             <p><strong>Born:</strong> {props.birth}</p>
-            <p><strong>Height:</strong> {props.height/100} Meters / {(props.height/30.5).toFixed(1)} Feet </p>
-            <p><strong>Weight:</strong> {props.weight} Kilograms / {(props.weight*2.2).toFixed(1)} Pounds </p>
+            <p><strong>Height:</strong> {props.height/100} Meters</p>
+            <p><strong>Weight:</strong> {props.weight} Kilograms / {props.weight*2.205%1}Pounds </p>
         </Card>
     );
 }
