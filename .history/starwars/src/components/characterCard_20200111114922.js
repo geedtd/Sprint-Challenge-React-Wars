5904c2f9f129e40c5b02778b0a6aa27cb794c156
Gerardo Cazares <grcazares@gmail.com>
2020-01-11 11:57:54 -0800

    Revert "Revert "Final""
    This reverts commit 5f5dda402cf85d5e6ee6fee6e2d4b52fa6231f33.

--- .history/starwars/src/components/characterCard_20200111114922.js
+++ .history/starwars/src/components/characterCard_20200111114922.js
@@ -18,7 +18,7 @@ const CharCard = props => {
         <Card>
             <p><strong>Name:</strong> {props.name}</p>
             <p><strong>Born:</strong> {props.birth}</p>
-            <p><strong>Height:</strong> {props.height/100} Meters / {(props.height/30.5).toFixed(1)} Feet </p>
+            <p><strong>Height:</strong> {props.height/100} Meters / {(props.height/30.5).toFixed(1)} Feet {((props.height/30.5)%1)*0.0833}inches</p>
             <p><strong>Weight:</strong> {props.weight} Kilograms / {(props.weight*2.2).toFixed(1)} Pounds </p>
         </Card>
     );
